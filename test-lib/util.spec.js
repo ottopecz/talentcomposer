2d7e9164a28a690e4ff7f6c76e2d458396c6fe7d
Otto Pecz <otto.pecz@gmail.com>
2019-01-10 20:21:25 +0000

    Making utils generic

--- test-lib/util.spec.js
+++ test-lib/util.spec.js
@@ -8,39 +8,39 @@ describe("The \"findWhere\" function", () => {
 
   it("should return an object element of the array where the given key value pair is present", () => {
 
-    const arr = [{"foo": "bar"}, {"foo": "notBar"}, {"bar": "foo"}];
+    const arr = [{"foo": "bar"}, {"oof": "rab", "bar": "foo"}];
 
-    expect(findWhere(arr, "foo", "bar")).equal({"foo": "bar"});
+    expect(findWhere(arr, "bar", "foo")).equal({"oof": "rab", "bar": "foo"});
   });
-});
-
-describe("The \"findWhere\" function", () => {
 
   it("should return undefined if the given key value pair is not present in any of the objects", () => {
 
-    const arr = [{"foo": "bar"}];
+    const arr = [{"oof": "rab"}, {"foo": "bar"}];
+
+    expect(findWhere(arr, "bar", "foo")).to.be.undefined();
+  });
+
+  it("should return undefined if only the key is present in a record and the value is not", () => {
+
+    const arr = [{"oof": "rab"}, {"bar": "bar"}];
 
     expect(findWhere(arr, "bar", "foo")).to.be.undefined();
   });
 });
 
-
 describe("The \"findWhereKey\" function", () => {
 
   it("should return an object element of the array where the given key is present", () => {
 
-    const arr = [{"foo": "bar"}, {"foo": "notBar"}, {"bar": "foo"}];
+    const arr = [{"foo": "bar"}, {"oof": "rab", "bar": "foo"}];
 
-    expect(findWhereKey(arr, "foo")).equal({"foo": "bar"});
+    expect(findWhereKey(arr, "bar")).equal({"oof": "rab", "bar": "foo"});
   });
-});
-
-describe("The \"findWhereKey\" function", () => {
 
   it("should return undefined if the given key is not present in any of the objects", () => {
 
-    const arr = [{"foo": "bar"}];
+    const arr = [{"foo": "bar"}, {"oof": "rab", "bar": "foo"}];
 
-    expect(findWhereKey(arr, "bar")).to.be.undefined();
+    expect(findWhereKey(arr, "rab")).to.be.undefined();
   });
 });
